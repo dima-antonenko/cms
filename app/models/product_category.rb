2c1b312def9623ad06ca28b406079a21ed3320ae
dima-antonenko <d.antonenko94@mail.ru>
2015-03-20 20:03:51 +0300

    cutomizing header

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,4 +2,7 @@ class ProductCategory < ActiveRecord::Base
 	has_many :product_categories
 	belongs_to :product_category
 	has_many :products
+
+	has_attached_file :image, :styles => { :medium => "340x280>", :thumb => "137x137>" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
