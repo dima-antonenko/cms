c104eb4f6ed8d48cb4ea8dc4188799a296946984
dima-antonenko <d.antonenko94@mail.ru>
2015-04-18 09:33:53 +0300

    add minicart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
   	request.referrer
   end
+
+  def minicart
+  	if @cart.id == session[:cart_id]
+  		'zz'
+  	end	
+  end
 end
