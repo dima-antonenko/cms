e1ced132ae56148b385fe8667dba285f135c3517
dima-antonenko <d.antonenko94@mail.ru>
2015-04-22 22:55:59 +0300

    update capfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: "d.antonenko94@mail.ru", encrypted_password: "$2a$10$2K.HHIYgUGfuM6QlLYgIQOaRZE1KR5onMcWcXiLFR7fPaWQswrrmO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: "2015-04-21 13:18:33", sign_in_count: 40, current_sign_in_at: "2015-04-21 13:18:33", last_sign_in_at: "2015-04-20 14:28:28", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", created_at: "2015-03-12 07:03:28", updated_at: "2015-04-21 13:18:33"
+User.create(email: "d.antonenko94@mail.ru", encrypted_password: "$2a$10$2K.HHIYgUGfuM6QlLYgIQOaRZE1KR5onMcWcXiLFR7fPaWQswrrmO")
