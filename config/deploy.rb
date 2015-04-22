898ae031e70b6e133ea04510ec6c86f5fedd6cf5
dima-antonenko <d.antonenko94@mail.ru>
2015-04-22 23:44:24 +0300

    final deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,85 +1,16 @@
-# По умолчанию для дистрибуции проектов используется Bundler.
-# Эта строка включает автоматическое обновление и установку
-# недостающих gems, указанных в вашем Gemfile.
-#
-## !!! Не забудьте добавить
-# gem 'capistrano'
-# gem 'unicorn'
-#
-# в ваш Gemfile.
-#
-# Если вы используете другую систему управления зависимостями,
-# закомментируйте эту строку.
-require 'bundler/capistrano'
+set :application, 'laston.dima-antonenko.lclients.ru'
+set :repo_url, 'https://github.com/dima-antonenko/cms.git'
 
-## Чтобы не хранить database.yml в системе контроля версий, поместите
-## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
-## следующие строки.
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
- after "deploy:update_code", :copy_database_config
- task :copy_database_config, roles => :app do
-   db_config = "#{shared_path}/database.yml"
-   run "cp #{db_config} #{release_path}/config/database.yml"
- end
+set :deploy_to, '/home/hosting_dima-antonenko/projects/laston'
 
-# В rails 3 по умолчанию включена функция assets pipelining,
-# которая позволяет значительно уменьшить размер статических
-# файлов css и js.
-# Эта строка автоматически запускает процесс подготовки
-# сжатых файлов статики при деплое.
-# Если вы не используете assets pipelining в своем проекте,
-# или у вас старая версия rails, закомментируйте эту строку.
-load 'deploy/assets'
+set :linked_files, %w{config/database.yml}
 
-# Для удобства работы мы рекомендуем вам настроить авторизацию
-# SSH по ключу. При работе capistrano будет использоваться
-# ssh-agent, который предоставляет возможность пробрасывать
-# авторизацию на другие хосты.
-# Если вы не используете авторизацию SSH по ключам И ssh-agent,
-# закомментируйте эту опцию.
-ssh_options[:forward_agent] = true
+role :app, %w{dima-antonenko@laston.dima-antonenko.lclients.ru}
 
-# Имя вашего проекта в панели управления.
-# Не меняйте это значение без необходимости, оно используется дальше.
-set :application,     "laston"
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Сервер размещения проекта.
-set :deploy_server,   "sulfur.locum.ru"
-
-# Не включать в поставку разработческие инструменты и пакеты тестирования.
-set :bundle_without,  [:development, :test]
-
-set :user,            "hosting_dima-antonenko"
-set :login,           "dima-antonenko"
-set :use_sudo,        false
-set :deploy_to,       "/home/#{user}/projects/#{application}"
-set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
-set :unicorn_pid,     "/var/run/unicorn/#{user}/#{application}.#{login}.pid"
-set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
-role :web,            deploy_server
-role :app,            deploy_server
-role :db,             deploy_server, :primary => true
-
-# Следующие строки необходимы, т.к. ваш проект использует rvm.
-set :rvm_ruby_string, "2.2.0"
-set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake" 
-set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
-
-# Настройка системы контроля версий и репозитария,
-# по умолчанию - git, если используется иная система версий,
-# нужно изменить значение scm.
-set :scm,             :git
-
-# Предполагается, что вы размещаете репозиторий Git в вашем
-# домашнем каталоге в подкаталоге git/<имя проекта>.git.
-# Подробнее о создании репозитория читайте в нашем блоге
-# http://locum.ru/blog/hosting/git-on-locum
-#set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{application}.git"
-
-## Если ваш репозиторий в GitHub, используйте такую конфигурацию
-set :repository,    "https://github.com/dima-antonenko/cms.git"
-
-## --- Ниже этого места ничего менять скорее всего не нужно ---
 
 before 'deploy:finalize_update', 'set_current_release'
 task :set_current_release, :roles => :app do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-role :app, %w{deploy@laston.dima-antonenko.lclients.ru}
-role :web, %w{deploy@laston.dima-antonenko.lclients.ru}
-role :db,  %w{deploy@laston.dima-antonenko.lclients.ru}
+role :app, %w{dima-antonenko@laston.dima-antonenko.lclients.ru}
+role :web, %w{dima-antonenko@laston.dima-antonenko.lclients.ru}
+role :db,  %w{dima-antonenko@laston.dima-antonenko.lclients.ru}
